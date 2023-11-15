cd39a4f0d2998eb3d06318bdbf97cd89e24a2f50
out0f4mm0 <eriking2004@gmail.com>
2023-11-15 14:23:46 +0100

    Nearly ended

--- js/app.js
+++ js/app.js
@@ -6,11 +6,13 @@ const result = document.querySelector("#resultado")
 
 const validate = (e) =>{
     if (e.target.value !== ""){
-        enable()
+        cleanseAlert(e.target.parentElement)
+        enableSubmitBtn()
     }else {
-        console.log("The field cant be empty")
+        showError("The field cant be empty", e.target.parentElement)
     }
 }
+
 const getApiData = (city, country) =>{
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}\`
                         &appid=3b2fa0825392ef439f6265ce4c7c5987`
@@ -19,11 +21,28 @@ const getApiData = (city, country) =>{
         .then(data => console.log(data))
         .catch((error) => console.error(error))
 }
+
 form.addEventListener("submit", (e) => {
     e.preventDefault()
     getApiData(cityField.value, countryField.value)
 })
-const enable = (qualifiedName, value) =>{
+
+const cleanseAlert = (reference) =>{
+    const alert = reference.querySelector(".bg-red-600")
+    if(alert){
+        alert.remove()
+    }
+}
+
+const showError = (errorMsg, reference) =>{
+    cleanseAlert(reference)
+    const error = document.createElement("P")
+    error.textContent = errorMsg
+    error.classList.add("bg-red-600", "text-center", "text-white", "p-2")
+    reference.appendChild(error)
+}
+
+const enableSubmitBtn = (qualifiedName, value) =>{
     if (countryField.value !== "" && cityField.value !== ""){
         submitBtn.removeAttribute("disabled")
         submitBtn.classList.remove('opacity-50')
@@ -32,5 +51,14 @@ const enable = (qualifiedName, value) =>{
         submitBtn.classList.add('opacity-50')
     }
 }
+
+const showWeatherOnHtml = (weatherData) =>{
+    const maxTemp = kelvinToCelsiusConverter(weatherData.main.max)
+}
+
+const kelvinToCelsiusConverter = (kelvinTemperature) => {
+    return kelvinTemperature - 273.15
+}
+
 cityField.addEventListener("blur", validate)
 countryField.addEventListener("input", validate)
